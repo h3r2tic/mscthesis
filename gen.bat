bf25fd1ce8adedf17cb5d88865dead1130de54e3
Tomasz Stachowiak <h3@h3.gd>
2010-08-17 10:07:28 +0100

    bib wrangling

--- gen.bat
+++ gen.bat
@@ -1,1 +1,12 @@
+del Thesis.bbl
+del Thesis.blg
+del Thesis.aux
+del Thesis.pdf
+del Thesis.out
+del Thesis.toc
+del Thesis.log
 xelatex Thesis.tex
+bibtex Thesis
+xelatex Thesis.tex
+xelatex Thesis.tex
+
